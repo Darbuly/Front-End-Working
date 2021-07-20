acb8f0123e0016824889cfb77355829eaacd9f02
Darbuly <xiaowei515@foxmail.com>
2021-07-20 10:32:01 +0800

    味仓PC端

--- project/weicang/market/market.js
+++ project/weicang/market/market.js
@@ -1,3 +1,4 @@
+
 $(function () {
     var curr_item_indx = 0;
 
@@ -11,24 +12,49 @@ $(function () {
         $('#product_layer').addClass('zoomProduct');
         $('#product_layer').removeClass('anti_ZoomProduct');
 
+        if (document.body.clientWidth <= 750) {
+            big_p_w = document.body.clientWidth * 0.8036;
+            $('.closer_phone')[0].style.display = 'block';
+        }
+
     };
 
-    product_item_click = function (indx) {
 
+
+    product_item_click = function (indx) {
+        var product_infos = readProductInfos();
+        if (indx >= product_infos.length
+            || indx < 0) {
+            console.log('触底了')
+            return;
+        }
         curr_item_indx = indx;
+
 
         $("#product_contain .product_item").eq(indx).addClass("productItemCur").siblings().removeClass('productItemCur');
         $("#hd ul li").eq(indx).addClass("on").siblings().removeClass('on');
 
-        var p_w = $("#product_layer").width();
-        var l = (p_w - 600) / 2 - 200 * indx;
+
+
+        var p_w = $("#product_layer").width();//总长
+        var one_p_w = 200;//单位长
+        var big_p_w = 600;
+
+        if (document.body.clientWidth <= 750) {
+            big_p_w = document.body.clientWidth * 0.8036;
+        }
+
+
+        console.log(`${one_p_w},${big_p_w}`);
+        var l = (p_w - big_p_w) / 2 - one_p_w * indx;
+        // var l = (p_w - 600) / 2 - 200 * indx;
         //商品条向左移动
         $("#product_contain").css({
             'left': l,
             'transition': 'left .3s ease'
         });
 
-        var product_infos = readProductInfos();
+
         //读取商品信息，覆写信息
         $("#product_infos .product_name").html(product_infos[indx].name);
         $("#product_infos .product_desc").html(product_infos[indx].desc);
@@ -38,6 +64,52 @@ $(function () {
         var product_infos = readProductInfos();
         window.open(product_infos[curr_item_indx].herf_url);
     };
+
+
+    activeDrag = () => {
+        console.log('移动端支持拖拽');
+        let position = [0, 0];
+
+        const div = document.getElementById("product_contain");
+        if (!div) {
+            throw new Error('拖动对象初始化失败，请检查id值是否正确');
+        }
+        div.addEventListener('touchstart', function (e) {
+            if (e.targetTouches.length == 1) {
+                position[0] = e.targetTouches[0].pageX;
+            }
+
+        }, false);
+
+        div.addEventListener('touchend', function (e) {
+            if (e.changedTouches.length == 1) {
+                let deltaX = e.changedTouches[0].pageX - position[0];
+                if (Math.abs(deltaX) < 10) {
+                    console.log(Math.abs(deltaX))
+                    return;
+                }
+                if (deltaX > 0) {
+                    //向右拖
+                    console.log('向右拖')
+                    setTimeout(() => {
+                        product_item_click(curr_item_indx - 1);
+                    }, 0)
+
+
+                } else {
+                    //向左拖
+                    console.log('向左拖')
+                    setTimeout(() => {
+                        product_item_click(curr_item_indx + 1);
+                    }, 0)
+
+                }
+            }
+
+
+        }, false);
+
+    }
 
     init();
 
@@ -75,7 +147,29 @@ $(function () {
 
         product_item_click(Math.floor(product_infos.length / 2));
         $('#product_layer').hide();
+        setTimeout(() => {
+            $('#border_animat').show();
+            $('#street_contain').css('display', 'flex')
+            $('#flash_title').css('display', 'none')
 
+        }, 2800)
+
+        //禁止浏览器拖动图片打开新标签页的默认事件
+        document.ondragover = function (e) { e.preventDefault(); };
+
+        //禁止浏览器拖动图片
+        var im = document.getElementsByTagName('img');
+        for (var i = 0; i < im.length; i++) {
+            im[i].onmousedown = function (e) {
+                e.preventDefault()
+            }
+        }
+
+
+        //移动端支持拖拽
+        if (document.body.clientWidth <= 750) {
+            activeDrag();
+        }
         // $('#street_contain').addClass('street_move');
 
         //初始化街道层
@@ -91,6 +185,7 @@ $(function () {
         //     $("#street_contain").append(str);
         // }
 
+
     }
 
     close_product_layer = () => {
@@ -102,7 +197,9 @@ $(function () {
         $('#street_contain').show();
         $('#m_logo_layer').show();
         // $('#border_animat').show();
+        $('.closer_phone')[0].style.display = 'none';
 
     }
+
 
 });
